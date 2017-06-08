b801591c832470b8c19d036d2c44770464614b69
David Levanon <david.levanon@takipi.com>
2017-06-08 15:19:10 +0300

    add custom script

--- scripts/big.sh
+++ scripts/big.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
 script_dir="$( cd "$( dirname "$0" )" && pwd )"
-cd $script_dir/..
 
-target_dir=`pwd`/big
-classes_count=1000
-
-echo "Generating to $target_dir"
-./gradlew run -Pconfig-class=ComplexConfig -Poutput-directory=$target_dir -Pclasses=$classes_count
-cd $target_dir
-
-echo "Compiling"
-gradle fatJar
-
-echo ""
-echo "Done"
-echo ""
-echo "To execute the new tester run:"
-echo ""
-echo "java -cp $target_dir/build/libs/tester.jar helpers.Main"
-echo ""
+$script_dir/custom.sh 1000 big
