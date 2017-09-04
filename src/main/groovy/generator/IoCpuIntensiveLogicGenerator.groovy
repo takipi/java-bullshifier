ed73a12afa9eabead163ea713989968ff012c9da
shimonmagal <shimon.magal@takipi.com>
2017-09-04 13:40:05 +0300

    repeat io cpu intensive logic

--- src/main/groovy/generator/IoCpuIntensiveLogicGenerator.groovy
+++ src/main/groovy/generator/IoCpuIntensiveLogicGenerator.groovy
@@ -6,151 +6,155 @@ import java.util.concurrent.Executors
 public class IoCpuIntensiveLogicGenerator {
 	static generate() {
 		return """
-		int maxFiles = $Config.ioCpuIntensiveFileLimit;
-		int matrixSize = $Config.ioCpuIntensiveMatrixSize;
 
-		if (matrixSize <= 0) {
-			return;
-		}
-		
-		// random matrix
-		int[][] matrix = new int[matrixSize][matrixSize];
-		java.util.Random rand = new java.util.Random();
-		for (int i = 0; i < matrixSize ; i++) {
-			for (int j = 0; j < matrixSize ; j++) {
-				matrix[i][j] = rand.nextInt();
+		for (int repeat = 0 ; repeat < $Config.ioCpuIntensiveRepeats ; repeat++) {
+
+			int maxFiles = $Config.ioCpuIntensiveFileLimit;
+			int matrixSize = $Config.ioCpuIntensiveMatrixSize;
+
+			if (matrixSize <= 0) {
+				return;
 			}
-		}
-	
-		// files
-		String directoryName = "output";
-		java.io.File directory = new java.io.File(directoryName);
-		directory.mkdirs();
-		String filePrefix = "matrix_" + java.lang.Thread.currentThread().currentThread().getId() + "_";
-		String pathPrefix = "output/" + filePrefix;
-		
- 		// find largest file number
-		int largestFileIndex = -1;
-		java.io.File[] files = directory.listFiles();
-		for (java.io.File file : files) {
-			if (file.getName().startsWith(filePrefix))
-			{
-				String fileSuffixNumber = file.getName().substring(filePrefix.length());
-				int number = Integer.parseInt(fileSuffixNumber);
-				
-				if (largestFileIndex < number) {
-					largestFileIndex = number;
+
+			// random matrix
+			int[][] matrix = new int[matrixSize][matrixSize];
+			java.util.Random rand = new java.util.Random();
+			for (int i = 0; i < matrixSize ; i++) {
+				for (int j = 0; j < matrixSize ; j++) {
+					matrix[i][j] = rand.nextInt();
 				}
 			}
-		}
 
-		// init default old matrix
-		int[][] oldMatrix = new int[matrixSize][matrixSize];
-		for (int i = 0; i < matrixSize; i++) {
-			for (int j = 0 ; j < matrixSize ; j++) {
-				oldMatrix[i][j] = 0;
-				
-				if (i == j) {
-					oldMatrix[i][j] = 1;
+			// files
+			String directoryName = "output";
+			java.io.File directory = new java.io.File(directoryName);
+			directory.mkdirs();
+			String filePrefix = "matrix_" + java.lang.Thread.currentThread().currentThread().getId() + "_";
+			String pathPrefix = "output/" + filePrefix;
+
+	 		// find largest file number
+			int largestFileIndex = -1;
+			java.io.File[] files = directory.listFiles();
+			for (java.io.File file : files) {
+				if (file.getName().startsWith(filePrefix))
+				{
+					String fileSuffixNumber = file.getName().substring(filePrefix.length());
+					int number = Integer.parseInt(fileSuffixNumber);
+
+					if (largestFileIndex < number) {
+						largestFileIndex = number;
+					}
 				}
 			}
-		}
-		
-		// read random old matrix
-		int randomOldFileIndex = 0;
 
-		if (largestFileIndex > 0) {
-			randomOldFileIndex = rand.nextInt(largestFileIndex);
-		}
+			// init default old matrix
+			int[][] oldMatrix = new int[matrixSize][matrixSize];
+			for (int i = 0; i < matrixSize; i++) {
+				for (int j = 0 ; j < matrixSize ; j++) {
+					oldMatrix[i][j] = 0;
+					
+					if (i == j) {
+						oldMatrix[i][j] = 1;
+					}
+				}
+			}
+			
+			// read random old matrix
+			int randomOldFileIndex = 0;
 
-		java.io.File oldFile = new java.io.File(pathPrefix + (randomOldFileIndex));
+			if (largestFileIndex > 0) {
+				randomOldFileIndex = rand.nextInt(largestFileIndex);
+			}
 
-		if ((largestFileIndex > 0) && (oldFile.exists())) {
-			java.io.BufferedReader br = null;
+			java.io.File oldFile = new java.io.File(pathPrefix + (randomOldFileIndex));
+
+			if ((largestFileIndex > 0) && (oldFile.exists())) {
+				java.io.BufferedReader br = null;
+				
+				try {
+					br = new java.io.BufferedReader(new java.io.FileReader(oldFile));
+					String line = br.readLine();
+					int lineNumber = 0 ;
+					
+					while(line != null) {
+						String[] splitted = line.split(" ");
+						
+						if (splitted.length != matrixSize) {
+							break;
+						}
+						
+						for (int i = 0 ; i < matrixSize ; i++) {
+							oldMatrix[i][lineNumber] = java.lang.Integer.parseInt(splitted[i]);
+						}
+						
+						lineNumber++;
+						if (lineNumber >= matrixSize) {
+							break;
+						}
+
+						line = br.readLine();
+					}
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				finally	{
+					if (br != null)
+					{
+						try
+						{
+							br.close();
+						}
+						catch (Exception e)
+						{
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+			
+			// multiply matrices
+			int[][] resultMatrix = new int[matrixSize][matrixSize];
+			
+			for (int i = 0 ; i < matrixSize ; i++) {
+				for (int j = 0 ; j < matrixSize ; j++) {
+					int res = 0;
+					for (int k = 0 ; k < matrixSize ; k++) {
+						res += oldMatrix[i][k] * matrix[k][j];
+					}
+					
+					resultMatrix[i][j] = res;
+				}
+			}
+			
+			// write new matrix
+			int newIndex = ((largestFileIndex + 1) % maxFiles);
+
+			java.io.File file = new java.io.File(pathPrefix + (newIndex));
+			java.io.BufferedWriter bw = null;
 			
 			try {
-				br = new java.io.BufferedReader(new java.io.FileReader(oldFile));
-				String line = br.readLine();
-				int lineNumber = 0 ;
-				
-				while(line != null) {
-					String[] splitted = line.split(" ");
-					
-					if (splitted.length != matrixSize) {
-						break;
+				bw = new java.io.BufferedWriter(new java.io.FileWriter(file));
+				for (int i = 0; i < matrixSize; i++) {
+					for (int j = 0; j < matrixSize; j++) {
+						bw.write(java.lang.Integer.toString(resultMatrix[i][j]));
+						bw.write(" ");
 					}
-					
-					for (int i = 0 ; i < matrixSize ; i++) {
-						oldMatrix[i][lineNumber] = java.lang.Integer.parseInt(splitted[i]);
-					}
-					
-					lineNumber++;
-					if (lineNumber >= matrixSize) {
-						break;
-					}
-
-					line = br.readLine();
+					bw.newLine();
+					bw.flush();
 				}
 			}
 			catch (Exception e) {
 				e.printStackTrace();
 			}
-			finally	{
-				if (br != null)
-				{
-					try
-					{
-						br.close();
+			finally {
+				if (bw != null) {
+					try {
+						bw.close();
 					}
-					catch (Exception e)
-					{
+					catch (Exception e) {
 						e.printStackTrace();
 					}
-				}
-			}
-		}
-		
-		// multiply matrices
-		int[][] resultMatrix = new int[matrixSize][matrixSize];
-		
-		for (int i = 0 ; i < matrixSize ; i++) {
-			for (int j = 0 ; j < matrixSize ; j++) {
-				int res = 0;
-				for (int k = 0 ; k < matrixSize ; k++) {
-					res += oldMatrix[i][k] * matrix[k][j];
-				}
-				
-				resultMatrix[i][j] = res;
-			}
-		}
-		
-		// write new matrix
-		int newIndex = ((largestFileIndex + 1) % maxFiles);
-
-		java.io.File file = new java.io.File(pathPrefix + (newIndex));
-		java.io.BufferedWriter bw = null;
-		
-		try {
-			bw = new java.io.BufferedWriter(new java.io.FileWriter(file));
-			for (int i = 0; i < matrixSize; i++) {
-				for (int j = 0; j < matrixSize; j++) {
-					bw.write(java.lang.Integer.toString(resultMatrix[i][j]));
-					bw.write(" ");
-				}
-				bw.newLine();
-				bw.flush();
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-		finally {
-			if (bw != null) {
-				try {
-					bw.close();
-				}
-				catch (Exception e) {
-					e.printStackTrace();
 				}
 			}
 		}
