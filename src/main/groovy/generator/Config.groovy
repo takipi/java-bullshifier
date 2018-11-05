7f5b4c56f6e831bf6df959fd89e916f2f92d4ad2
Hodaya Gamliel <hodaya.gamliel@takipi.com>
2018-11-05 12:58:30 +0200

    naming

--- src/main/groovy/generator/Config.groovy
+++ src/main/groovy/generator/Config.groovy
@@ -30,5 +30,5 @@ public class Config {
     
     public static ioCpuIntensiveMatrixSize = 300
     public static ioCpuIntensiveFileLimit = 100
-    public static shouldLogicCode = true 
+    public static shouldGenerateLogicCode = true 
 }
