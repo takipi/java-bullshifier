23ea29915717b5c1ba6596157493c7d0947deef3
Hodaya Gamliel <hodaya.gamliel@takipi.com>
2021-04-12 12:21:30 +0300

    minute-->minutes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-/opt/overops/$COLOR/run.sh --run-in-container \
-        --running-hours $RUNNING_DURATION_HOURS \
-        --running-minutes $RUNNING_DURATION_MINUTE
+command="/opt/overops/$COLOR/run.sh --run-in-container "
+
+if [[ -n "${RUNNING_DURATION_HOURS}" ]]; then 
+        command+="--running-hours $RUNNING_DURATION_HOURS "
+fi
+
+if [[ -n "${RUNNING_DURATION_MINUTES}" ]]; then 
+        command+="--running-minutes $RUNNING_DURATION_MINUTES"
+fi
+
+echo "About to run:"
+echo "$command"
+
+if ! $command ; then 
+	echo "Failed to run java bullshifier (command: $command)"
+	exit 1
+fi
